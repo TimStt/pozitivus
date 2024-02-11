b6525763a34044bd9def765010733c2ef2ffb9b7
TimStt <timoshka200004@gmail.com>
2024-02-11 13:11:10 +0300

    web

--- js/main.js
+++ js/main.js
@@ -1,40 +1,66 @@
-const swiper = new Swiper(".swiper", {
-  centeredSlides: true,
+function main() {
+  const sliderRender = () => {
+    const swiper = new Swiper(".swiper", {
+      centeredSlides: true,
 
-  spaceBetween: 50,
-  watchOverflow: true,
-  updateOnWindowResize: true,
+      spaceBetween: 50,
+      watchOverflow: true,
+      updateOnWindowResize: true,
 
-  navigation: {
-    nextEl: ".button-next",
-    prevEl: ".button-prev",
-  },
-  breakpoints: {
-    500: {
-      slidesPerView: "auto",
-    },
-    1000: {
-      slidesPerView: 2,
-    },
+      navigation: {
+        nextEl: ".button-next",
+        prevEl: ".button-prev",
+      },
+      breakpoints: {
+        500: {
+          slidesPerView: "auto",
+        },
+        1000: {
+          slidesPerView: 2,
+        },
 
-    1400: {
-      slidesPerView: 2,
-    },
-  },
-});
+        1400: {
+          slidesPerView: 2,
+        },
+      },
+    });
 
-const paginationItems = document.querySelectorAll(".pagination__button"); // находим элементы пагинации
+    const paginationRender = () => {
+      const paginationItems = document.querySelectorAll(".pagination__button"); // находим элементы пагинации
 
-swiper.on("slideChange", function () {
-  paginationItems.forEach(function (item, index) {
-    index === swiper.realIndex
-      ? item.classList.add("is-active")
-      : item.classList.remove("is-active");
-  });
-});
+      swiper.on("slideChange", function () {
+        paginationItems.forEach(function (item, index) {
+          index === swiper.realIndex
+            ? item.classList.add("is-active")
+            : item.classList.remove("is-active");
+        });
+      });
 
-paginationItems.forEach(function (item, index) {
-  item.addEventListener("click", function () {
-    swiper.slideTo(index);
-  });
-});
+      const paginationHandlerClick = () => {
+        paginationItems.forEach(function (item, index) {
+          item.addEventListener("click", function () {
+            swiper.slideTo(index);
+          });
+        });
+      };
+      paginationHandlerClick();
+    };
+
+    paginationRender();
+  };
+
+  const burgerMenuHandler = () => {
+    const burger_btn = document.querySelector(".burger-wrapper");
+    let menuIsVisible = false;
+
+    burger_btn.addEventListener("click", () => {
+      menuIsVisible = !menuIsVisible;
+      burger_btn.classList.toggle("menu-is-visible");
+      document.body.style.overflow = menuIsVisible ? "hidden" : "visible";
+    });
+  };
+
+  burgerMenuHandler();
+  sliderRender();
+}
+main();
